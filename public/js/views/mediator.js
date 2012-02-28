edcadbdf3f64abdb73e8fc312d4fe723b20a93b6
Erwin Mombay <berwin9@yahoo.com>
2012-02-27 23:43:50 -0500

    removed unneeded comments

--- public/js/views/mediator.js
+++ public/js/views/mediator.js
@@ -63,9 +63,6 @@ define(function(require) {
     });
 
     mediator.on('rightClick', function(spec) {
-        //spec.event.stopPropagation();
-        //: doing a return false on the on.contextmenu event
-        //: prevents the default browser's contextmenu to pop up
     });
 
     mediator.on('doubleClick:composite', function(spec) {
