a15a59ad0a56ad4b113609e00e4e7cfe13d3f5a4
Erwin Mombay <berwin9@yahoo.com>
2012-03-03 00:17:25 -0500

    fixed cascade destroy

--- public/js/models/ComponentModel.js
+++ public/js/models/ComponentModel.js
@@ -1,19 +1,30 @@
 define(function(require) {
+    'use strict';
     var $ = require('jquery');
     var _ = require('underscore');
     var Backbone = require('backbone');
 
     var ComponentModel = Backbone.Model.extend({
-        initialize: function(options) {
-            _.bindAll(this, 'validate');
-            //: TODO remove options when we finally pass in a true spec
-            this.blueprint = options && options.blueprint;
+        initialize: function(attr) {
+            var self = this;
+            _.bindAll(this, 'destroy');
+            if (attr && attr.componentCollection) {
+                this.componentCollection = attr.componentCollection;
+            }
+            socket.emit('test', { my: this.cid });
+            socket.on('response', function(socket) {
+                self.cid = socket.my;
+                self.set('label', socket.my);
+            });
         },
 
-        validate: function() {
-            if (this.blueprint && this.blueprint.valdation) {
-                this.blueprint.validation.call(this);
+        destroy: function(options) {
+            if (this.componentCollection && (options && options.cascade)) {
+                for (var i = 0, l = this.componentCollection.length; i < l; i++) {
+                    this.componentCollection.models[0].destroy();
+                }
             }
+            Backbone.Model.prototype.destroy.call(this, options);
         }
     });
     return ComponentModel;
