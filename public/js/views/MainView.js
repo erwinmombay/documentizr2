d4c3cb9f598ab2246938457bedfcaace53b62548
Erwin Mombay <berwin9@yahoo.com>
2012-03-03 19:15:43 -0500

    cleaned up schema builder and broke some parts into functions
    - still need to fix nested loops

--- public/js/views/MainView.js
+++ public/js/views/MainView.js
@@ -29,10 +29,6 @@ define(function(require) {
 
         render: function() {
             $('.sidebar-nav').append(this.doctree.el);
-            //var $preT = $('<pre/>', { 'class': 'prettyprint' });
-            //var $codeT = $('<code/>', { 'class': 'language-js' });
-            //$codeT.append(JSON.stringify(bootstrapData, null, 4));
-            //$('.span9').append($preT.append($codeT));
             var $pre = $('<pre/>', { 'class': 'prettyprint' });
             var $code = $('<code/>', { 'class': 'language-js' });
             var data = this.buildDocLevelSchema(bootstrapData);
@@ -59,20 +55,67 @@ define(function(require) {
         },
         
         buildTableLevelSchema: function(curTable, curTableSegments) {
-            //: make a copy of the original array
-            var queue = curTableSegments.slice(0);
+            //: make a copy of the original array and reverse the order
+            var queue = curTableSegments.slice(0).reverse();
+            //: push any loop object we create to this array so we can
+            //: keep appending to it for any segment that is a part of the loop
+            var loops = [];
+
             while(queue.length) {
-                //: pop the first item
-                var curItem = queue.shift();
-                _.each(queue, function(value) {
-                    curTable.collection[curItem.segment] = {};
-                    this.buildSegmentLevelSchema();
-                }, this);
+                var curItem = queue.pop();
+                //: if not part of a loop directly attach to the table
+                if (curItem.parent_loop_pos === 'n/a' && curItem.loop === 'None') {
+                    //: meeting the conditions on the above if statement means
+                    //: it is safe to dequeue the last loop if any exists because we
+                    //: are now outside any nesting and the curItem is directly
+                    //: under the table.
+                    this.popAppend(loops, curTable);
+                    curTable.collection[curItem.segment] = this.buildSegment(curItem);
+                //: else if under a loop
+                } else if (curItem.parent_loop_pos === 'n/a') {
+                    var curLoop = loops[loops.length - 1];
+                    //: check if a loop exists
+                    if (curLoop) {
+                        if (curLoop.initiator === curItem.loop) {
+                            curLoop.collection[curItem.segment] = this.buildSegment(curItem);
+                        }
+                    //: else create the loop
+                    } else {
+                        var loop = this.buildLoop(curItem);
+                        //: the loop initiator also needs to have an entry
+                        //: inside the loop (ex. N1 segment inside Loop_N1
+                        loop.collection[curItem.segment] = this.buildSegment(curItem);
+                        loops.push(loop); 
+                    }
+                } else {
+                    this.popAppend(loops, curTable);
+                }
             }
         },
 
-        buildSegmentLevelSchema: function(node, data) {
-            
+        popAppend: function(popTarget, appendTarget) {
+            var dequeuedItem = popTarget.pop();
+            if (dequeuedItem) {
+                appendTarget.collection[dequeuedItem.name] = dequeuedItem;
+            }
+        },
+
+        buildSegment: function(curItem) {
+            return {
+                name: curItem.segment,
+                posNo: curItem.pos_no,
+                maxOccurs: curItem.max_count
+            };
+        },
+
+        buildLoop: function(curItem) {
+            return {
+                name: 'Loop_' + curItem.segment,
+                posNo: curItem.pos_no,
+                initiator: curItem.segment,
+                maxOccurs: curItem.loop_rep,
+                collection: {}
+            };
         }
     });
 
