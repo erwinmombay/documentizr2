ad170d1c0c1121be4e1a1dbf33a05085aa78d3c1
Erwin Mombay <berwin9@yahoo.com>
2012-03-03 23:48:15 -0500

    semi functional buildTableLevelSchema, need to fix

--- public/js/views/MainView.js
+++ public/js/views/MainView.js
@@ -63,8 +63,12 @@ define(function(require) {
 
             while(queue.length) {
                 var curItem = queue.pop();
+                var loop = null;
+                var queuedLoop = loops.length ? loops[loops.length - 1] : null;
                 //: if not part of a loop directly attach to the table
+                console.log(curItem.segment + ' ' + curItem.pos_no);
                 if (curItem.parent_loop_pos === 'n/a' && curItem.loop === 'None') {
+                    console.log('1');
                     //: meeting the conditions on the above if statement means
                     //: it is safe to dequeue the last loop if any exists because we
                     //: are now outside any nesting and the curItem is directly
@@ -73,36 +77,70 @@ define(function(require) {
                     curTable.collection[curItem.segment] = this.buildSegment(curItem);
                 //: else if under a loop
                 } else if (curItem.parent_loop_pos === 'n/a') {
-                    var curLoop = loops[loops.length - 1];
+                    console.log('2');
                     //: check if a loop exists
-                    if (curLoop) {
-                        if (curLoop.initiator === curItem.loop) {
-                            curLoop.collection[curItem.segment] = this.buildSegment(curItem);
+                    if (queuedLoop) {
+                    console.log('2-a');
+                        if (queuedLoop.initiator === curItem.loop) {
+                            queuedLoop.collection[curItem.segment] = this.buildSegment(curItem);
+                            queuedLoop = null;
+                        } else {
+                    console.log('2-b');
+                            loop = this.buildLoop(curItem);
+                            loop.collection[curItem.segment] = this.buildSegment(curItem);
+                            loops.push(loop); 
+                            this.popAppend(loops, curTable);
+                            loop = null;
                         }
                     //: else create the loop
                     } else {
-                        var loop = this.buildLoop(curItem);
-                        //: the loop initiator also needs to have an entry
-                        //: inside the loop (ex. N1 segment inside Loop_N1
+                    console.log('2-c');
+                        loop = this.buildLoop(curItem);
                         loop.collection[curItem.segment] = this.buildSegment(curItem);
-                        loops.push(loop); 
+                        loops.push(loop);
+                        loop = null;
                     }
+                } else if (curItem.parent_loop_pos == queuedLoop.posNo) {
+                    console.log('3');
+                    var nestedLoop = this.buildLoop(curItem);
+                    console.log(JSON.stringify(nestedLoop));
+                    queuedLoop.collection[nestedLoop.fullName] = nestedLoop;
+                    nestedLoop.collection[curItem.segment] = this.buildSegment(curItem);
+                    loops.push(nestedLoop);
+                    queuedLoop = null;
                 } else {
-                    this.popAppend(loops, curTable);
+                    console.log('4');
+                    var siblingQueuedLoop = loops.length && loops[loops.length - 2];
+                    if (queuedLoop && siblingQueuedLoop && queuedLoop.parentPosNo === siblingQueuedLoop.posNo &&
+                        curItem.loop === queuedLoop.initiator) {
+                            console.log('4-a');
+                            queuedLoop.collection[curItem.segment] = this.buildSegment(curItem);
+                            queuedLoop = null;
+                    } else if (queuedLoop && siblingQueuedLoop && queuedLoop.parentPosNo === siblingQueuedLoop.posNo) {
+                        console.log('4-b');
+                        this.popAppend(loops, siblingQueuedLoop);
+                    } else {
+                        this.popAppend(loops, curTable); 
+                    }
                 }
+            }
+            //: attach the main loop under a table(if it exists) when the queue is exhausted
+            if (loops.length) {
+                this.popAppend(loops, curTable); 
             }
         },
 
         popAppend: function(popTarget, appendTarget) {
             var dequeuedItem = popTarget.pop();
             if (dequeuedItem) {
-                appendTarget.collection[dequeuedItem.name] = dequeuedItem;
+                appendTarget.collection[dequeuedItem.fullName] = dequeuedItem;
             }
         },
 
         buildSegment: function(curItem) {
             return {
                 name: curItem.segment,
+                fullName: curItem.segment + '_' + curItem.pos_no,
                 posNo: curItem.pos_no,
                 maxOccurs: curItem.max_count
             };
@@ -111,8 +149,10 @@ define(function(require) {
         buildLoop: function(curItem) {
             return {
                 name: 'Loop_' + curItem.segment,
+                fullName: 'Loop_' + curItem.segment + '_' + curItem.pos_no,
                 posNo: curItem.pos_no,
                 initiator: curItem.segment,
+                parentPosNo: curItem.parent_loop_pos,
                 maxOccurs: curItem.loop_rep,
                 collection: {}
             };
