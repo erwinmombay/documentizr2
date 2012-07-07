8ad7bd4f9b4f5f6c0cb9c2fe4f80aa27af3e19ca
Erwin Mombay <berwin9@yahoo.com>
2012-07-07 19:55:34 -0400

    updated build for new paths

--- app.build.js
+++ app.build.js
@@ -6,20 +6,17 @@
     optimize: 'uglify',
     paths: {
         order: 'libs/require/order.min',
-        jquery: 'libs/jquery/jquery-1.7.1.min',
+        jquery: 'libs/jquery/jquery-1.7.2.min',
         jqueryui: 'libs/jquery/jquery-ui-1.8.18.custom.min',
         underscore: 'libs/underscore/underscore.min',
         backbone: 'libs/backbone/backbone.min',
         handlebars: 'libs/handlebars/handlebars',
-        text: 'libs/require/text.min',
+        text: 'libs/require/text',
         templates: '../templates',
         models: 'models',
         collections: 'collections',
         utils: 'utils',
         modal: 'libs/bootstrap/bootstrap-modal'
-    },
-    modules: [{
-        name: 'main'
-    }]
+    }
 })
 
