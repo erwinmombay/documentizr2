e16ac328087608e7ae6c2c58141508fb43b77966
Erwin Mombay <berwin9@yahoo.com>
2012-08-05 21:49:50 -0400

    fixed spec runners to work with require 2.0

---
+++ public/js/tests/specs/main.js
@@ -0,0 +1,40 @@
+require.config({
+    baseUrl: '../',
+    paths: {
+        jquery: 'libs/jquery/jquery-1.7.2.min',
+        jqueryui: 'libs/jquery/jquery-ui-1.8.18.custom.min',
+        underscore: 'libs/underscore/underscore.min',
+        backbone: 'libs/backbone/backbone.min',
+        handlebars: 'libs/handlebars/handlebars',
+        text: 'libs/require/text',
+        templates: '../templates',
+        models: 'models',
+        collections: 'collections',
+        utils: 'utils',
+        modal: 'libs/bootstrap/bootstrap-modal'
+    }
+});
+
+//: we use the order plugin the load the library's synchronously
+/*global define: true*/
+define(function(require) {
+    'use strict';
+    var $ = require('jquery');
+    var _ = require('underscore');
+    var Backbone = require('backbone');
+    var text = require('text');
+    var jqui = require('jqueryui');
+    var handlebars = require('handlebars');
+    var modal = require('modal');
+
+    require([
+        'tests/specs/mediator.spec',
+        'tests/specs/models/ComponentModel.spec',
+        'tests/specs/collections/ComponentCollection.spec',
+        'tests/specs/views/TreeView.spec'
+    ], function() {
+        jasmine.getEnv().addReporter(new jasmine.HtmlReporter());
+        jasmine.getEnv().execute();
+    });
+});
+
