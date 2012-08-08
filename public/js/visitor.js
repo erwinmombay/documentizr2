ffb5143eed7e56646d22987e4e5740c35b8cb651
Erwin Mombay <berwin9@yahoo.com>
2012-08-07 23:10:40 -0400

    added visitor class to be tracked

---
+++ public/js/visitor.js
@@ -0,0 +1,30 @@
+/*global define:true, $:true, Backbone:true, _:true*/
+define(function() {
+    var TsetVisitor = {
+        root: undefined,
+        currentNode: undefined,
+        depth: undefined,
+        pos: undefined,
+
+        setTarget: function() {
+            
+        },
+
+        parent: function() {
+
+        },
+
+        child: function() {
+            
+        },
+
+        prev: function() {
+
+        },
+
+        next: function() {
+
+        }
+    };
+    return TsetVisitor;
+});
