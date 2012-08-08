748a12cf820d63c04d0fd72e4342a076fb443e5a
Erwin Mombay <berwin9@yahoo.com>
2012-08-08 01:35:32 -0400

    added private properties only accessible through getters

--- public/js/visitor.js
+++ public/js/visitor.js
@@ -1,30 +1,49 @@
 /*global define:true, $:true, Backbone:true, _:true*/
 define(function() {
-    var TsetVisitor = {
-        root: undefined,
-        currentNode: undefined,
-        depth: undefined,
-        pos: undefined,
+    var TsetVisitor = (function() {
+        var _root, _currentNode, _depth, _pos;
+        return {
+            reset: function() {
+                _root = _currentNode = _pos = _depth = undefined;
+            },
 
-        setTarget: function() {
+            setTarget: function(target) {
+                _root = _currentNode = target;
+                _pos = _depth = 0;
+            },
+
+            getRoot: function() {
+                return _root;
+            },
+
+            getNode: function() {
+                return _currentNode;
+            },
             
-        },
+            getDepth: function() {
+                return _depth;
+            },
 
-        parent: function() {
+            getPosition: function() {
+                return _pos;
+            },
 
-        },
+            parent: function() {
 
-        child: function() {
-            
-        },
+            },
 
-        prev: function() {
+            child: function() {
+                
+            },
 
-        },
+            prev: function() {
 
-        next: function() {
+            },
 
-        }
-    };
+            next: function() {
+
+            }
+        };
+    }());
     return TsetVisitor;
 });
